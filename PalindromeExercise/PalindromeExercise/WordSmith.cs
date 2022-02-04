45e14d56a6a78f9941ec84d51bf1d0e96d1bf627
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-02-04 13:20:46 -0500

    Finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+       
+            public bool isAPalindrome(string text)
+            {
+            var result = Enumerable
+             .SequenceEqual(text.ToLower().ToCharArray(), text.ToLower().ToCharArray()
+             .Reverse());
+            return result;
+        }
+
+        
+
+
+
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+    { 
 
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("radar", true)]
+        [InlineData("car", false)]
+        [InlineData("Radar", true)]
+        public void Test1(string word, bool expected)
+        {
+            //arrange
+            var test = new WordSmith();
+            //act
+            var actual = test.isAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
